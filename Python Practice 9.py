191c1f1b84e8b4b41ed90a2b867af8d10d9dce41
Maria Aqdas <mariaaqdas786@gmail.com>
2026-08-18 22:02:06 +0500

    Update Python Practice 9.py

--- Python Practice 9.py
+++ Python Practice 9.py
@@ -5,15 +5,12 @@
 
 class Student:
     pass
-
 # Creating two objects from the Student class
 student1 = Student()
 student2 = Student()
-
 print("Q1: Class and Objects")
 print(type(student1))
 print(type(student2))
-
 print("-" * 50)
 
 # Q2. STUDENT ATTRIBUTES
